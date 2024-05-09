bcded3e1a97de379dee55cd6365904a3cc99e42b
Lenndemaster <lenn.kenis@student.ucll.be>
2024-05-09 15:22:25 +0200

    Buttononclick toegevoegd

--- FEGevorderd_LoopLegends/src/app/buttons/buttons.component.ts
+++ FEGevorderd_LoopLegends/src/app/buttons/buttons.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-buttons',
-  standalone: true,
-  imports: [],
   templateUrl: './buttons.component.html',
-  styleUrl: './buttons.component.css'
 })
 export class ButtonsComponent {
-
+  onButtonClick() {
+    alert('Button clicked!');
+  }
 }
