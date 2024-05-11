d0830be87ec0734bf6b476dfc9eacdbfbdb3de6b
Lenn Kenis <lenn.kenis@student.ucll.be>
2024-05-11 02:41:57 +0200

    Buttons, zijbalk en eindafrekening gemaakt. Als je op een button klikt komt dit in een winkelmandje en daarna Wordt dit allemaal opgeteld bij totale kosten

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -6,14 +6,36 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  clickedButtons: { [key: string]: { count: number, price: number } } = {};
   clickedButtonTitle: string = '';
+  totalCost: number = 0;
 
-  showTitle(buttonTitle: string): void {
-    this.clickedButtonTitle = buttonTitle;
+  showTitle(category: string, price: number) {
+    if (!this.clickedButtons[category]) {
+      this.clickedButtons[category] = { count: 1, price: price };
+    } else {
+      this.clickedButtons[category].count++;
+    }
+
+    this.updateTotalCost(); // Update de totale kosten
+    this.updateClickedButtonTitle(); // Update de weergave van de geklikte knoppen
+  }
+
+  private updateTotalCost() {
+    this.totalCost = Object.keys(this.clickedButtons).reduce((acc, key) => {
+      const item = this.clickedButtons[key];
+      return acc + item.price * item.count;
+    }, 0);
+  }
+
+  private updateClickedButtonTitle() {
+    this.clickedButtonTitle = Object.keys(this.clickedButtons).map(key => {
+      const item = this.clickedButtons[key];
+      const count = item.count > 1 ? `${item.count}X` : '';
+      return `${count} ${key} €${item.price}`;
+    }).join('<br>');
   }
 }
-
-
