358bf1ded2680dfe2e3dc9cd9d52d3ddb5d37be5
Sander <sander.vanderstraeten@skynet.be>
2024-06-10 15:02:24 +0200

    bon

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { DrinkenComponent } from './drinken/drinken.component';
 import { HoofdgerechtenComponent } from './hoofdgerechten/hoofdgerechten.component';
 import { TussendoortjesComponent } from './tussendoortjes/tussendoortjes.component';
 import { DessertenComponent } from './desserten/desserten.component';
-
+import { BonComponent } from './bon/bon.component';
 
 export const routes: Routes = [
-    { path: 'drinken', component: DrinkenComponent },
-    { path: 'hoofdgerechten', component: HoofdgerechtenComponent },
-    { path: 'tussendoortjes', component: TussendoortjesComponent },
-    { path: 'desserten', component: DessertenComponent },
-    { path: '', redirectTo: '/drinken', pathMatch: 'full' },
-    { path: '**', redirectTo: '/drinken', pathMatch: 'full' }
-
+  { path: 'drinken', component: DrinkenComponent },
+  { path: 'hoofdgerechten', component: HoofdgerechtenComponent },
+  { path: 'tussendoortjes', component: TussendoortjesComponent },
+  { path: 'desserten', component: DessertenComponent },
+  { path: 'bon', component: BonComponent },
+  { path: '', redirectTo: '/drinken', pathMatch: 'full' },
+  { path: '**', redirectTo: '/drinken', pathMatch: 'full' },
 ];
