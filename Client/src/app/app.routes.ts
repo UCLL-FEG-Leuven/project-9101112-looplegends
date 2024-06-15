76811da54dca67e345a9b6c096a5e005da29ac60
Sander <sander.vanderstraeten@skynet.be>
2024-06-15 17:49:36 +0200

    login pg gemaakt

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -6,14 +6,13 @@ import { DessertenComponent } from './desserten/desserten.component';
 import { BonComponent } from './bon/bon.component';
 import { LoginComponent } from './login/login.component';
 
-
 export const routes: Routes = [
   { path: 'drinken', component: DrinkenComponent },
   { path: 'hoofdgerechten', component: HoofdgerechtenComponent },
   { path: 'tussendoortjes', component: TussendoortjesComponent },
   { path: 'desserten', component: DessertenComponent },
   { path: 'bon', component: BonComponent },
+  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/drinken', pathMatch: 'full' },
   { path: '**', redirectTo: '/drinken', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
 ];
